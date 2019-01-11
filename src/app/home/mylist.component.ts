f5b9402f55b420056e5987561ab4dbf08371325e
Moses Johansson <moses.johansson@preciofishbone.se>
2019-01-11 09:30:01 +0100

    example of list with different colors

--- src/app/home/mylist.component.ts
+++ src/app/home/mylist.component.ts
@@ -7,6 +7,8 @@ import { Observable } from "rxjs";
 import { UserService } from "../services/user.service";
 import { getBoolean, setBoolean } from "tns-core-modules/application-settings";
 import { SearchBar } from "tns-core-modules/ui/search-bar";
+import { ChangeDetectionStrategy } from "@angular/core";
+import { SetupItemViewArgs } from "nativescript-angular/directives";
 
 import { 
     firestore, 
@@ -14,6 +16,25 @@ import {
     login as firebaseLogin} from "nativescript-plugin-firebase"
 const firebase = require("nativescript-plugin-firebase")
 const firebase2 = require("nativescript-plugin-firebase/app");
+
+class Country {
+    constructor(public name: string) { }
+}
+
+let europianCountries = ["Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic",
+    "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy",
+    "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+    "Slovenia", "Spain", "Sweden", "United Kingdom"];
+
+class Item {
+    constructor(public name: string) { }
+}
+
+let items = ["ALL Heroes (header)", "Razor", "Rubick", "Phantom Lancer", "Legion Commander", "Brewmaster",
+    "Outworld Devourer", "Sniper", "Lina", "Sven", "Visage", "Undying", "Tiny", "Tidehunter", "Puck", "Ursa",
+    "Magnus", "Earthshaker", "Windrunner", "Techies", "Crystal Maiden", "Batrider", "Riki", "Invoker", "Venomancer",
+    "Timbersaw", "Wraithking", "Anti Mage", "Ancient Apparition", "Troll Warlord", "Lich", "Enchantress",
+    "Bristleback", "Pudge", "(footer)"];
 
 @Component({
     selector: "Mylist",
@@ -30,12 +51,39 @@ export class MylistComponent implements OnInit {
     user: User;
     users$: Observable<Array<User>>;
 
-    constructor(private userService: UserService) {
-        // Use the component constructor to inject providers.
-    }
     ngOnInit(): void {
         
         
+    }
+
+    public countries: Array<Country>;
+    public dataItems: Array<Item>;
+
+    constructor() {
+        this.countries = [];
+
+        for (let i = 0; i < europianCountries.length; i++) {
+            this.countries.push(new Country(europianCountries[i]));
+            if(i == 5)
+            {
+                this.countries[5].name = 'hello this is a long string because who the hell cares... aijdia\njsdijada';   
+            }
+        }
+
+        this.dataItems = [];
+
+        for (let i = 0; i < items.length; i++) {
+            this.dataItems.push(new Item(items[i]));
+        }
+    }
+    onSetupItemView(args: SetupItemViewArgs) {
+        args.view.context.third = (args.index % 3 === 0);
+        args.view.context.header = ((args.index + 1) % items.length === 1);
+        args.view.context.footer = (args.index + 1 === items.length);
+    }
+
+    public onItemTap(args) {
+        console.log("Item Tapped at cell index: " + args.index);
     }
 
 }
